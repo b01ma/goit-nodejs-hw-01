6e675e9af1e077b8ba39572578f0ef9f4772684e
ed_bolma <ed.bolma21@gmail.com>
2023-03-14 11:51:31 +0100

    'Try..catch changed by the function that catches erroes'

--- contacts.js
+++ contacts.js
@@ -5,70 +5,61 @@ const { uuid } = require('uuidv4');
 
 const contactsPath = path.join('db', 'contacts.json');
 
-const contactList = async () => {
+const errorCatcher = fn => {
   try {
-    const result = await fs.readFile(contactsPath);
-    const contacts = JSON.parse(result.toString());
-
-    return contacts;
+    return fn;
   } catch (error) {
+    console.log('======error=====');
     console.log(error);
   }
+};
+
+const contactList = async () => {
+  const result = await errorCatcher(await fs.readFile(contactsPath));
+  const contacts = JSON.parse(result.toString());
+
+  return contacts;
 };
 
 // TODO: document each function
 
 const listContacts = async () => {
-  try {
-    const contacts = await contactList();
+  const contacts = await errorCatcher(await contactList());
 
-    console.log('=======================');
-    console.log(contacts);
-    console.log('=======================');
-  } catch (err) {
-    console.log(err);
-  }
+  console.log('=======================');
+  console.log(contacts);
+  console.log('=======================');
 };
 
 const getContactById = async contactId => {
-  try {
-    const contacts = await contactList();
+  const contacts = await errorCatcher(await contactList());
 
-    const contactById = contacts.find(
-      contact => contact.id === contactId.toString()
-    );
+  const contactById = contacts.find(
+    contact => contact.id === contactId.toString()
+  );
 
-    console.log('=======================');
-    console.log(contactById || 'There is no such contact');
-    console.log('=======================');
-  } catch (error) {
-    console.log(error);
-  }
+  console.log('=======================');
+  console.log(contactById || 'There is no such contact');
+  console.log('=======================');
 };
 
 const removeContact = async contactId => {
-  try {
-    const contacts = await contactList();
+  const contacts = await errorCatcher(await contactList());
 
-    const newArray = contacts.filter(contact => contact.id !== contactId);
+  const newArray = contacts.filter(
+    contact => contact.id !== contactId.toString()
+  );
 
-    await fs.writeFile(contactsPath, JSON.stringify(newArray));
-  } catch (error) {
-    console.log(error);
-  }
+  errorCatcher(await fs.writeFile(contactsPath, JSON.stringify(newArray)));
 };
 
 const addContact = async (name, email, phone) => {
-  try {
-    const contacts = await contactList();
-    const newContact = { id: uuid(), name, email, phone };
+  const contacts = await errorCatcher(await contactList());
+  const newContact = { id: uuid(), name, email, phone };
 
-    const newArray = [...contacts, newContact];
+  const newArray = [...contacts, newContact];
 
-    await fs.writeFile(contactsPath, JSON.stringify(newArray));
-  } catch (error) {
-    console.log(error);
-  }
+  errorCatcher(await fs.writeFile(contactsPath, JSON.stringify(newArray)));
 };
 
 module.exports = {
